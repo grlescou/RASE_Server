301159d63c2cb59ccdd323de303d919cb2eafa5e
Kathleen <Kathleen@Kathee-PC>
2016-02-15 08:01:30 -0500

    Implémentation de la classe Catégorie

--- src/main/java/ht/mbds/haiti/rase/model/model/Categorie.java
+++ src/main/java/ht/mbds/haiti/rase/model/model/Categorie.java
@@ -5,12 +5,81 @@
  */
 package ht.mbds.haiti.rase.model.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  *
  * @author gaetan
  */
+@Document(collection = "categorie")
 public class Categorie implements Serializable {
+    @Id
+    private String id;
+    private String nom;
+    private String description;
+    @DBRef
+    private Maladie maladie;
+
     
+    public Categorie() {
+    }
+    @JsonCreator
+    public Categorie(@JsonProperty("nom") String nom, @JsonProperty("description") String description) {
+        this.nom = nom;
+        this.description = description;
+       
+    }
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * @param nom the nom to set
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Categorie{" + "id=" + id + ", nom=" + nom + ", description=" + description +  '}';
+    }
+
 }
